053d576e3a7afb5a761259f3c478803c2b7b0fd7
Chantal Snazel <clsnazel@gmail.com>
2020-10-01 01:22:48 +0000

    init react app, getting things set up for meeting

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+///TODO: edit and style based on UI designs
+
+export default class Header {
+  
+  render() {
+    return(
+      <div>
+        <h1> Battle Ship!!!</h1>
+      </div>
+    );
+  }
+};
